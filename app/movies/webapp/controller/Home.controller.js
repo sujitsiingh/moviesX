ad02cf5ed5e63bf07222101eabb4c969187ee0de
Sujit Kumar Singh <sujitks85988@gmail.com>
2025-09-10 11:16:50 +0000

    changes in views,csv files

--- app/movies/webapp/controller/Home.controller.js
+++ app/movies/webapp/controller/Home.controller.js
@@ -1,10 +1,15 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/m/MessageToast",
+    "sap/ui/model/Sorter"
+], (Controller, Filter, FilterOperator, MessageToast, Sorter) => {
     "use strict";
 
     return Controller.extend("movies.controller.Home", {
-        onInit() {
+        onInit: function () {
+            
         }
     });
 });
