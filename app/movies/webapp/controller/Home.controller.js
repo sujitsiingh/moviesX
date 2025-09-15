e53d30bb1859c16b577ed9c0481b653624778cb7
Sujit Kumar Singh <sujitks85988@gmail.com>
2025-09-15 11:03:05 +0000

    Edit Functionality

--- app/movies/webapp/controller/Home.controller.js
+++ app/movies/webapp/controller/Home.controller.js
@@ -1,3 +1,6 @@
+const { update } = require("@sap/cds");
+const { func } = require("@sap/cds/lib/ql/cds-ql");
+
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
@@ -34,11 +37,20 @@ sap.ui.define([
             oPanel.setVisible(true);
         },
 
+        // <---- edit press => open a panel 3 ---->
+        onEditPressed: function () {
+            this.hideAllPanels();
+            var oPanel = this.byId("panel3");
+            oPanel.setVisible(true);
+        },
+
         hideAllPanels: function () {
             const oPanel = this.byId("panel1");
             oPanel.setVisible(false);
-            const oPanel1 = this.byId("panel2");
-            oPanel1.setVisible(false);
+            const oPanel2 = this.byId("panel2");
+            oPanel2.setVisible(false);
+            const oPanel3 = this.byId("panel3");
+            oPanel3.setVisible(false);
         },
 
 
@@ -126,7 +138,82 @@ sap.ui.define([
                     }
                 }
             });
+        },
+
+        onEditPress: function () {
+            var oData = this._oSelectedContext.getObject();
+            MessageToast.show("Edit your entries for Movie ID: " +oData.ID);
+            this.onEditPressed();  // ->panel func will triger
+            var pdt_model = this.getOwnerComponent().getModel();
+            let aFilters = [
+                new Filter("ID", FilterOperator.EQ, oData.ID)
+            ];
+            let oBind = pdt_model.bindList("/Movies");
+            oBind.filter(aFilters);
+
+            oBind.requestContexts().then((aContexts) => {
+                // handle retrieved contexts..
+                if(aContexts.length > 0) {
+                    aContexts.forEach(oContext => {
+                        let oUser = oContext.getObject();
+                        this.getView().byId("editTitle").setValue(oUser.title);
+                        this.getView().byId("editOverview").setValue(oUser.overview);
+                        this.getView().byId("editReleaseYear").setValue(oUser.releaseYear);
+                        this.getView().byId("editRunTime").setValue(oUser.runtimeMin);
+                        this.getView().byId("editReviews").setValue(oUser.numReviews);
+                        this.getView().byId("editCast").setValue(oUser.cast);
+                        this.getView().byId("editCurrency").setValue(oUser.currency_code);
+                        this.getView().byId("itemCode").setValue(oUser.ID);
+                    });
+                } else {
+                    MessageBox.error("No Movie found with specified ID");
+                }
+            })
+            .catch(e => {
+                MessageBox.error("Error retrieving Movies details: " + e)
+            });
+
+        },
+
+        onEditMe: function () {
+            var itemCode = this.getView().byId("itemCode").getValue();
+
+            var title = this.getView().byId("editTitle").getValue();
+            var overview = this.getView().byId("editOverview").getValue();
+            var releaseYear = this.getView().byId("editReleaseYear").getValue();
+            var runtimeMin = this.getView().byId("editRunTime").getValue();
+            var numReviews = this.getView().byId("editReviews").getValue();
+            var cast = this.getView().byId("editCast").getValue();
+            var currency = this.getView().byId("editCurrency").getValue();
+
+            var update_oModel = this.getView().getModel();
+            var sPath = "/Movies('"+itemCode+"')";
+            var oContext = update_oModel.BindContext(sPath).getBoundContext();
+
+            var oView = this.getView();
+            function resetBusy() {
+                oView.setBusy(false);
+            }
+            oView.setBusy(true);
+
+            oContext.setProperty("title", title);
+            oContext.setProperty("overview", overview);
+            oContext.setProperty("releaseYear", releaseYear);
+            oContext.setProperty("runtimeMin", runtimeMin);
+            oContext.setProperty("numReviews", numReviews);
+            oContext.setProperty("cast", cast);
+            oContext.setProperty("currency", currency);
+
+            update_oModel.submitBatch("auto").then(function () {
+                resetBusy();
+                MessageBox.success("Movie details updated successfully!");
+            })
+            .catch(function (e) {
+                resetBusy();
+                MessageBox.error("An error occured while updating details..:" +e);
+            })
         }
+
 
     });
 });
